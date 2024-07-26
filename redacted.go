44314146175be4e27a5fc4b8f72a848669ea32dc
Andrew Heberle <andrew.heberle@gmail.com>
2024-07-26 15:10:36 +0800

    Additional comments

--- redacted.go
+++ redacted.go
@@ -3,6 +3,7 @@ package stringutil
 // Redacted can be used to print a redacted string using fmt.Println(Redacted("this will be printed as all *"))
 type Redacted string
 
+// RedactedCharacter is the default character used to redact a Redacted string
 var RedactedCharacter = '*'
 
 func (r Redacted) String() string {
@@ -14,6 +15,7 @@ func (r Redacted) String() string {
 	return string(b)
 }
 
+// Redact returns the provided string with all characters replaced with "c"
 func Redact(s string, c rune) string {
 	b := make([]byte, 0)
 	for range s {
